02eebcd1abc3c125fa33a700af5184672bdf61d9
mocenas <mocenas@redhat.com>
2019-01-29 10:40:22 +0100

    fix uploading results
    Uploaded results need to be addressed using a  triple.
    Also the OpenShift secret for upload needs to be added
    to the FMP `deployment.yml` file, not just `build.yml`.

--- src/main/java/com/github/rhoar_ci/dashboard/storage/JobService.java
+++ src/main/java/com/github/rhoar_ci/dashboard/storage/JobService.java
@@ -2,26 +2,27 @@ package com.github.rhoar_ci.dashboard.storage;
 
 import com.github.rhoar_ci.dashboard.ci.TestCluster;
 import com.github.rhoar_ci.dashboard.ci.TestDescription;
+import com.github.rhoar_ci.dashboard.ci.TestType;
 import com.github.rhoar_ci.dashboard.jenkins.Job;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.*;
 
 @ApplicationScoped
 public class JobService {
-    private Map<Pair<TestCluster, TestDescription>, Job> jobStorage= new HashMap<>();
+    private Map<Triple<TestCluster, TestDescription, TestType>, Job> jobStorage= new HashMap<>();
 
     public void updateJob(Job jenkinsJob){
-        Pair<TestCluster, TestDescription> pair = jobToPair(jenkinsJob);
-        jobStorage.put(pair, jenkinsJob);
+        Triple<TestCluster, TestDescription, TestType> triple = jobToTriple(jenkinsJob);
+        jobStorage.put(triple, jenkinsJob);
     }
 
     public Collection<Job> getAll(){
         return jobStorage.values();
     }
 
-    private Pair<TestCluster, TestDescription> jobToPair(Job jenkinsJob){
-        return Pair.of(jenkinsJob.getCluster(), jenkinsJob.getDescription());
+    private Triple<TestCluster, TestDescription, TestType> jobToTriple(Job jenkinsJob){
+        return Triple.of(jenkinsJob.getCluster(), jenkinsJob.getDescription(), jenkinsJob.getType());
     }
 }
