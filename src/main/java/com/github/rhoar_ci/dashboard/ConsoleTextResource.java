0c0d270b949d4305e7288b7a1c675f53ccce3984
Ladislav Thon <lthon@redhat.com>
2019-04-12 14:30:40 +0200

    convert Jenkins job output to colorized HTML

--- src/main/java/com/github/rhoar_ci/dashboard/ConsoleTextResource.java
+++ src/main/java/com/github/rhoar_ci/dashboard/ConsoleTextResource.java
@@ -1,6 +1,7 @@
 package com.github.rhoar_ci.dashboard;
 
 import com.github.rhoar_ci.dashboard.jenkins.JenkinsClient;
+import org.fusesource.jansi.HtmlAnsiOutputStream;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -8,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.StreamingOutput;
+import java.io.PrintWriter;
 
 @Path("/console-text/{buildName}/{buildNumber}")
 public class ConsoleTextResource {
@@ -19,8 +21,24 @@ public class ConsoleTextResource {
     private JenkinsClient jenkins;
 
     @GET
-    @Produces("text/plain;charset=utf-8") // as Jenkins responds in utf-8 too
+    @Produces("text/html;charset=utf-8") // Jenkins responds in UTF-8 too
     public StreamingOutput get(@PathParam("buildName") String buildName, @PathParam("buildNumber") String buildNumber) {
-        return out -> jenkins.streamConsoleText(buildName, buildNumber).writeTo(out);
+        return out -> {
+            PrintWriter writer = new PrintWriter(out);
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("  <meta charset=\"utf-8\">");
+            writer.println("  <title>" + buildName + " #" + buildNumber + "</title>");
+            writer.println("</head>");
+            writer.println("<body style=\"color: #f3f3f3; background-color: #272822\">");
+            writer.println(" <pre>");
+            writer.flush();
+            jenkins.streamConsoleText(buildName, buildNumber).writeTo(new HtmlAnsiOutputStream(out));
+            out.flush();
+            writer.println(" </pre>");
+            writer.println("</body>");
+            writer.println("</html>");
+            writer.flush();
+        };
     }
 }
